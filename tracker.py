21e904447442387592df0e081829bb63fbad9ac5
Daniel <scalena99@gmail.com>
2021-03-29 17:04:47 +0200

    still not working

--- tracker.py
+++ tracker.py
@@ -11,6 +11,7 @@ import time
 from time import sleep
 
 
+
 def check(url):
     print(f'Checking {url}')
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
