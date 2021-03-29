48516f81be6e77ec4d712def8e4961bd3ec150a8
Daniel <scalena99@gmail.com>
2021-03-29 17:03:26 +0200

    still not working

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ I simply need a Nvidia RTX 3080 and I'm building this to track availability on A
 This script run in background on a Raspberry PI and (should) send me a notification when a RTX 3080 is available on Amazon as fast as possible
 
 Update: I haven't been able to buy a 3080 yet
+
+
+## How it works
+
--- tracker.py
+++ tracker.py
@@ -1,7 +1,68 @@
-# hello
+# Hi, nice to meet u. My name is Daniel
+
+# ASIN of 3080s: https://en.wikipedia.org/wiki/Amazon_Standard_Identification_Number
+
+
+import pandas as pd
+from lxml import html
+from bs4 import BeautifulSoup
+import requests
+import time
+from time import sleep
+
+
+def check(url):
+    print(f'Checking {url}')
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+
+    page = requests.get(url, headers = headers)
+
+    soup = BeautifulSoup(page.content, features="lxml")
+    sleep(1)
+    try:
+        title = soup.find(id='productTitle').get_text().strip()
+    except:
+        title = 'No Title'
+    
+    print(f'Product title: {title}')
+    # to prevent script from crashing when there isn't a price for the product
+    try:
+        price = float(soup.find(id='priceblock_ourprice').get_text().replace('.', '').replace('€', '').replace(',', '.').strip())
+    except:
+        price = ''
+    
+    try:
+        stock = soup.find(id='productTitle')[0].get_text().strip()
+    except:
+        stock = 'Qualcosa e\' andato storto mentre controllavo'
+
+    return stock, price
+    
+
+
+def main():
+    print('\n\n')
+    print('Welcomeeeee')
+    print()
+
+    eighties = pd.read_csv('asins.csv')
+    eighties.columns = ['ASIN', 'link']
+
+    for ele in eighties['ASIN']:
+        availability, price = check(url = 'https://www.amazon.it/dp/' + str(ele))
+        print(f'Stock: {availability}')
+        print(f'Price: {price}\n')
 
 
 
 
 
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
 
